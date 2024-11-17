4b7158fca18efbf15ba98f30b4d02f067ee4e68a
niravpandey2005 <nrvpandey2005@gmail.com>
2024-11-18 10:33:52 +1100

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleSidebar() {
+    const sidebar = document.getElementById("sidebar");
+    if (sidebar.style.width === "0px" || sidebar.style.width === "") {
+        sidebar.style.width = "250px";
+    } else {
+        sidebar.style.width = "0px";
+    }
+};
+
